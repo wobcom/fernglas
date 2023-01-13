b228935f45ad9d522060f9800f130e5fe200d402
Yureka <yuka@yuka.dev>
2023-01-13 14:00:23 +0100

    fix input on start page (again)

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -1,6 +1,8 @@
 import { html, render } from 'lit-html';
 import { go } from './router.js';
 
+const modes = ["MostSpecific", "Exact", "OrLonger", "Contains"];
+
 const formSubmit = (e) => {
 	e.preventDefault();
 	const data = new FormData(e.target);
@@ -21,8 +23,8 @@ const formSubmit = (e) => {
 export const searchTemplate = (query) => html`
 	<form id="input" @submit=${formSubmit}>
 		<select name="query-mode" id="query-mode" @change=${() => document.getElementById("input-submit").click()}>
-			${["MostSpecific", "Exact", "OrLonger", "Contains"].map(name => html`
-				<option value=${name} ?selected=${query[0] === name}>${name}</option>
+			${modes.map(name => html`
+				<option value=${name} ?selected=${query && query[0] === name}>${name}</option>
 			`)}
 		</select>
 		<input name="input-field" id="input-field" type="text" spellcheck=false" autocomplete="new-password" autocorrect="off" autocapitalize="off" value=${query ? `${query[1]}/${query[2]}` : ''} />
