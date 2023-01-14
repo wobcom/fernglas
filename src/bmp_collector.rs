a1d727905b791275e30cef5a7c551c856f85039d
Yureka <yuka@yuka.dev>
2023-01-14 13:05:13 +0100

    restructure bmp_collector

--- src/bmp_collector.rs
+++ src/bmp_collector.rs
@@ -1,5 +1,8 @@
 use futures_util::StreamExt;
+use bitvec::view::BitView;
+use bitvec::prelude::Msb0;
 use std::net::IpAddr;
+use std::net::SocketAddr;
 use ipnet::{IpNet, Ipv4Net, Ipv6Net};
 use tokio_util::codec::length_delimited::LengthDelimitedCodec;
 use tokio::net::TcpListener;
@@ -14,6 +17,9 @@ use zettabgp::prelude::BgpLargeCommunityList;
 use zettabgp::prelude::BgpCommunityList;
 use zettabgp::prelude::BgpAddr;
 use zettabgp::bmp::BmpMessage;
+use zettabgp::bmp::prelude::BmpMessageRouteMonitoring;
+use zettabgp::bmp::prelude::BmpMessagePeerHeader;
+use zettabgp::bmp::prelude::BmpMessagePeerUp;
 use zettabgp::prelude::BgpAttrItem;
 use crate::table::{Table, TableSelector, RouteAttrs, RouteOrigin, SessionId};
 
@@ -36,6 +42,112 @@ fn bgp_addrs_to_nets(addrs: &BgpAddrs) -> Vec<IpNet> {
         _ => {}
     }
     res
+}
+
+fn table_selector_for_peer(client_addr: SocketAddr, peer: &BmpMessagePeerHeader) -> Option<TableSelector> {
+    match (peer.peertype, peer.flags.view_bits::<Msb0>()[7]) {
+        (0, false) => Some(TableSelector::PrePolicyAdjIn(SessionId {
+            from_client: client_addr,
+            remote_router_id: peer.routerid,
+        })),
+        (0, true) => Some(TableSelector::PostPolicyAdjIn(SessionId {
+            from_client: client_addr,
+            remote_router_id: peer.routerid,
+        })),
+        (3, _) => Some(TableSelector::LocRib { from_client: client_addr }),
+        _ => None,
+    }
+}
+
+async fn process_route_monitoring(table: &impl Table, client_addr: SocketAddr, rm: BmpMessageRouteMonitoring) {
+    let session = match table_selector_for_peer(client_addr, &rm.peer) {
+        Some(session) => session,
+        None => {
+            eprintln!("unknown peer type {} flags {:x}", rm.peer.peertype, rm.peer.flags);
+            return;
+        }
+    };
+
+    let mut attrs: RouteAttrs = Default::default();
+    let mut nexthop = None;
+    let mut update_nets = vec![];
+    let mut withdraw_nets = vec![];
+    let log = format!("{:#?}", rm);
+    for attr in rm.update.attrs {
+        match attr {
+            BgpAttrItem::MPUpdates(updates) => {
+                let nexthop = match updates.nexthop {
+                    BgpAddr::V4(v4) => Some(IpAddr::from(v4)),
+                    BgpAddr::V6(v6) => Some(IpAddr::from(v6)),
+                    _ => None,
+                };
+                for net in bgp_addrs_to_nets(&updates.addrs) {
+                    update_nets.push((net, nexthop));
+                }
+            }
+            BgpAttrItem::MPWithdraws(withdraws) => {
+                for net in bgp_addrs_to_nets(&withdraws.addrs) {
+                    withdraw_nets.push(net);
+                }
+            }
+            BgpAttrItem::NextHop(BgpNextHop { value }) => {
+                nexthop = Some(value);
+            }
+            BgpAttrItem::CommunityList(BgpCommunityList { value }) => {
+                let mut communities = vec![];
+                for community in value.into_iter() {
+                    communities.push(((community.value >> 16) as u16, (community.value & 0xff) as u16));
+                }
+                attrs.communities = Some(communities);
+            }
+            BgpAttrItem::MED(BgpMED { value }) => {
+                attrs.med = Some(value);
+            }
+            BgpAttrItem::LocalPref(BgpLocalpref { value }) => {
+                attrs.local_pref = Some(value);
+            }
+            BgpAttrItem::Origin(BgpOrigin { value }) => {
+                attrs.origin = Some(match value {
+                    BgpAttrOrigin::Igp => RouteOrigin::Igp,
+                    BgpAttrOrigin::Egp => RouteOrigin::Egp,
+                    BgpAttrOrigin::Incomplete => RouteOrigin::Incomplete,
+                })
+            }
+            BgpAttrItem::ASPath(BgpASpath { value }) => {
+                let mut as_path = vec![];
+                for asn in value {
+                    as_path.push(asn.value);
+                }
+                attrs.as_path = Some(as_path);
+            }
+            BgpAttrItem::LargeCommunityList(BgpLargeCommunityList { value }) => {
+                let mut communities = vec![];
+                for community in value.into_iter() {
+                    communities.push((community.ga, community.ldp1, community.ldp2));
+                }
+                attrs.large_communities = Some(communities);
+            }
+            _ => {},
+        }
+    }
+    for net in bgp_addrs_to_nets(&rm.update.updates).into_iter() {
+        update_nets.push((net, nexthop));
+    }
+    for net in bgp_addrs_to_nets(&rm.update.withdraws).into_iter() {
+        withdraw_nets.push(net);
+    }
+
+    if update_nets.len() == 0 && withdraw_nets.len() == 0 {
+        println!("{}", log);
+    }
+    for (net, nexthop) in update_nets {
+        let mut attrs = attrs.clone();
+        attrs.nexthop = nexthop;
+        table.update_route(net, session.clone(), attrs).await;
+    }
+    for net in withdraw_nets {
+        table.withdraw_route(net, session.clone()).await;
+    }
 }
 
 pub async fn run(table: impl Table) -> anyhow::Result<()> {
@@ -68,103 +180,12 @@ pub async fn run(table: impl Table) -> anyhow::Result<()> {
                     }
                 };
 
-                if let BmpMessage::RouteMonitoring(rm) = msg {
-                    let session = match (rm.peer.peertype, (rm.peer.flags & 64) != 0) {
-                        (0, false) => TableSelector::PrePolicyAdjIn(SessionId {
-                            from_client: client_addr,
-                            remote_router_id: rm.peer.routerid,
-                        }),
-                        (0, true) => TableSelector::PostPolicyAdjIn(SessionId {
-                            from_client: client_addr,
-                            remote_router_id: rm.peer.routerid,
-                        }),
-                        (3, _) => TableSelector::LocRib { from_client: client_addr },
-                        _ => {
-                            eprintln!("unknown peer type {} flags {:x}", rm.peer.peertype, rm.peer.flags);
-                            continue;
-                        }
-                    };
-
-                    let mut attrs: RouteAttrs = Default::default();
-                    let mut nexthop = None;
-                    let mut update_nets = vec![];
-                    let mut withdraw_nets = vec![];
-                    for attr in rm.update.attrs {
-					    match attr {
-                            BgpAttrItem::MPUpdates(updates) => {
-                                let nexthop = match updates.nexthop {
-                                    BgpAddr::V4(v4) => Some(IpAddr::from(v4)),
-                                    BgpAddr::V6(v6) => Some(IpAddr::from(v6)),
-                                    _ => None,
-                                };
-                                for net in bgp_addrs_to_nets(&updates.addrs) {
-                                    update_nets.push((net, nexthop));
-                                }
-                            }
-                            BgpAttrItem::MPWithdraws(withdraws) => {
-                                for net in bgp_addrs_to_nets(&withdraws.addrs) {
-                                    withdraw_nets.push(net);
-                                }
-                            }
-                            BgpAttrItem::NextHop(BgpNextHop { value }) => {
-                                nexthop = Some(value);
-                            }
-                            BgpAttrItem::CommunityList(BgpCommunityList { value }) => {
-                                let mut communities = vec![];
-                                for community in value.into_iter() {
-                                    communities.push(((community.value >> 16) as u16, (community.value & 0xff) as u16));
-                                }
-                                attrs.communities = Some(communities);
-                            }
-                            BgpAttrItem::MED(BgpMED { value }) => {
-                                attrs.med = Some(value);
-                            }
-                            BgpAttrItem::LocalPref(BgpLocalpref { value }) => {
-                                attrs.local_pref = Some(value);
-                            }
-                            BgpAttrItem::Origin(BgpOrigin { value }) => {
-                                attrs.origin = Some(match value {
-                                    BgpAttrOrigin::Igp => RouteOrigin::Igp,
-                                    BgpAttrOrigin::Egp => RouteOrigin::Egp,
-                                    BgpAttrOrigin::Incomplete => RouteOrigin::Incomplete,
-                                })
-                            }
-                            BgpAttrItem::ASPath(BgpASpath { value }) => {
-                                let mut as_path = vec![];
-                                for asn in value {
-                                    as_path.push(asn.value);
-                                }
-                                attrs.as_path = Some(as_path);
-                            }
-                            BgpAttrItem::LargeCommunityList(BgpLargeCommunityList { value }) => {
-                                let mut communities = vec![];
-                                for community in value.into_iter() {
-                                    communities.push((community.ga, community.ldp1, community.ldp2));
-                                }
-                                attrs.large_communities = Some(communities);
-                            }
-                            _ => {},
-                        }
-					}
-                    for net in bgp_addrs_to_nets(&rm.update.updates).into_iter() {
-                        update_nets.push((net, nexthop));
+                match msg {
+                    BmpMessage::RouteMonitoring(rm) => {
+                        process_route_monitoring(&table, client_addr, rm).await;
                     }
-                    for net in bgp_addrs_to_nets(&rm.update.withdraws).into_iter() {
-                        withdraw_nets.push(net);
-                    }
-
-                    for (net, nexthop) in update_nets {
-                        let mut attrs = attrs.clone();
-                        attrs.nexthop = nexthop;
-                        table.update_route(net, session.clone(), attrs).await;
-                    }
-                    for net in withdraw_nets {
-                        table.withdraw_route(net, session.clone()).await;
-                    }
-                    continue;
+                    msg => println!("{} {:#?}", client_addr, msg),
                 }
-
-                println!("{:#?}", msg);
             }
         });
 
