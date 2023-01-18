c4174016e7f741e68de0c36c2d2f9b8fdfc5be50
Yureka <yuka@yuka.dev>
2023-01-18 18:55:26 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
   description = "fernglas";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: {
