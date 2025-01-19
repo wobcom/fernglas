9eb23290b38767e6b57fa2da9472b25d145df3c7
Yureka <yuka@yuka.dev>
2025-01-19 18:52:35 +0100

    InMemoryTableState

--- src/table_impl.rs
+++ src/table_impl.rs
@@ -36,9 +36,21 @@ impl<T: Compressable> Compressable for Action<T> {
     }
 }
 
+pub struct InMemoryTableState<T = CompressedRouteAttrs> {
+    pub table: Node<IpNet, Vec<(PathId, T)>>,
+}
+
+impl<T> Default for InMemoryTableState<T> {
+    fn default() -> Self {
+        Self {
+            table: Default::default(),
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct InMemoryTable<T: Compressable = RouteAttrs> {
-    pub table: Arc<Mutex<Node<IpNet, Vec<(PathId, T::Compressed)>>>>,
+    pub state: Arc<Mutex<InMemoryTableState<T::Compressed>>>,
     subscribers: Arc<Mutex<Vec<Weak<Subscriber<T::Compressed>>>>>,
     caches: Arc<Mutex<Caches>>,
 }
@@ -72,10 +84,65 @@ impl NodeExt for Node<IpNet, Vec<(PathId, Arc<CompressedRouteAttrs>)>> {
     }
 }
 
+impl<T: Clone + Send + Sync> std::iter::Extend<(IpNet, PathId, T)> for InMemoryTableState<T> {
+    fn extend<I>(&mut self, iter: I)
+    where
+        I: IntoIterator<Item = (IpNet, PathId, T)>,
+    {
+        for (net, num, attrs) in iter {
+            self.update_route(num, net, attrs);
+        }
+    }
+}
+impl<T: Clone + Send + Sync> std::iter::FromIterator<(IpNet, PathId, T)> for InMemoryTableState<T> {
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = (IpNet, PathId, T)>,
+    {
+        let mut table = Self::default();
+        table.extend(iter);
+        table
+    }
+}
+
+impl<T: Clone + Send + Sync> InMemoryTableState<T> {
+    pub fn update_route(&mut self, path_id: PathId, net: IpNet, attrs: T) {
+        let mut new_insert = None;
+        let entry = self.table.exact_mut(&net).unwrap_or_else(|| {
+            new_insert = Some(Vec::new());
+            new_insert.as_mut().unwrap()
+        });
+
+        match entry.binary_search_by_key(&path_id, |(k, _)| *k) {
+            Ok(index) => drop(std::mem::replace(&mut entry[index], (path_id, attrs))),
+            Err(index) => entry.insert(index, (path_id, attrs)),
+        };
+
+        if let Some(insert) = new_insert {
+            self.table.insert(&net, insert);
+        }
+    }
+
+    pub fn withdraw_route(&mut self, path_id: PathId, net: IpNet) {
+        let is_empty = match self.table.exact_mut(&net) {
+            Some(entry) => {
+                if let Ok(index) = entry.binary_search_by_key(&path_id, |(k, _)| *k) {
+                    entry.remove(index);
+                }
+                entry.is_empty()
+            }
+            None => return,
+        };
+        if is_empty {
+            self.table.remove(&net);
+        }
+    }
+}
+
 impl<C: Clone + Send + Sync, T: Compressable<Compressed = C>> InMemoryTable<T> {
     pub fn new(caches: Arc<Mutex<Caches>>) -> Self {
         Self {
-            table: Default::default(),
+            state: Default::default(),
             subscribers: Default::default(),
             caches,
         }
@@ -85,7 +152,8 @@ impl<C: Clone + Send + Sync, T: Compressable<Compressed = C>> InMemoryTable<T> {
         self.subscribers.lock().unwrap().push(cb);
     }
 
-    pub fn update_route_compressed(&self, path_id: PathId, net: IpNet, compressed: C) {
+    pub fn update_route(&self, path_id: PathId, net: IpNet, route: T) {
+        let compressed = route.compress(&self.caches);
         for subscriber in self
             .subscribers
             .lock()
@@ -96,27 +164,8 @@ impl<C: Clone + Send + Sync, T: Compressable<Compressed = C>> InMemoryTable<T> {
             (*subscriber)(net, path_id, Action::Update(compressed.clone()));
         }
 
-        let mut table = self.table.lock().unwrap();
-
-        let mut new_insert = None;
-        let entry = table.exact_mut(&net).unwrap_or_else(|| {
-            new_insert = Some(Vec::new());
-            new_insert.as_mut().unwrap()
-        });
-
-        match entry.binary_search_by_key(&path_id, |(k, _)| *k) {
-            Ok(index) => drop(std::mem::replace(&mut entry[index], (path_id, compressed))),
-            Err(index) => entry.insert(index, (path_id, compressed)),
-        };
-
-        if let Some(insert) = new_insert {
-            table.insert(&net, insert);
-        }
-    }
-
-    pub fn update_route(&self, path_id: PathId, net: IpNet, route: T) {
-        let compressed = route.compress(&self.caches);
-        self.update_route_compressed(path_id, net, compressed)
+        let mut state = self.state.lock().unwrap();
+        state.update_route(path_id, net, compressed);
     }
 
     pub fn withdraw_route(&self, path_id: PathId, net: IpNet) {
@@ -130,19 +179,7 @@ impl<C: Clone + Send + Sync, T: Compressable<Compressed = C>> InMemoryTable<T> {
             (*subscriber)(net, path_id, Action::Withdraw);
         }
 
-        let mut table = self.table.lock().unwrap();
-
-        let is_empty = match table.exact_mut(&net) {
-            Some(entry) => {
-                if let Ok(index) = entry.binary_search_by_key(&path_id, |(k, _)| *k) {
-                    entry.remove(index);
-                }
-                entry.is_empty()
-            }
-            None => return,
-        };
-        if is_empty {
-            table.remove(&net);
-        }
+        let mut state = self.state.lock().unwrap();
+        state.withdraw_route(path_id, net);
     }
 }
