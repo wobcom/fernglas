030f615a77f8ffc65d0ac7f44868959476d1278d
Yureka <yuka@yuka.dev>
2025-09-06 00:22:33 +0200

    Never display ipv4-mapped nexthop addresses
    This fixes the reverse dns resolution for ipv4-mapped nexthops

--- src/store.rs
+++ src/store.rs
@@ -310,7 +310,7 @@ pub trait Store: Clone + Send + Sync + 'static {
                         }
                     }
                     BgpAttrItem::NextHop(BgpNextHop { value }) => {
-                        nexthop = Some(value);
+                        nexthop = Some(value.to_canonical());
                     }
                     BgpAttrItem::CommunityList(BgpCommunityList { value }) => {
                         let mut communities = vec![];
@@ -364,7 +364,7 @@ pub trait Store: Clone + Send + Sync + 'static {
                     rd = session.route_distinguisher
                 }
                 let mut attrs = attrs.clone();
-                attrs.nexthop = nexthop;
+                attrs.nexthop = nexthop.map(|nh| nh.to_canonical());
                 self.update_route(
                     path,
                     prefix,
